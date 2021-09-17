f13a84527e3867fe2e6c35b05bf130dc1bfa627b
Olivier Estévez <oliviercillo@gmail.com>
2021-09-17 13:08:03 +0200

    Added an image import via PHP example

--- home.php
+++ home.php
@@ -1,11 +1,9 @@
 <?php
-
 defined( 'ABSPATH' ) || exit;
 
 get_header();
-
-the_content();
-
-get_footer();
-
 ?>
+
+<img src="<?php echo get_template_directory_uri() ?>/assets/imgs/space-banana.jpg" alt="">
+
+<?php get_footer(); ?>
